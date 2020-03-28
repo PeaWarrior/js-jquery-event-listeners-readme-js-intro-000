7ded35e98b891c277dd3ab9638d392710e177a48
Jackson Chen <jacksonchen171@gmail.com>
2020-03-28 21:42:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,36 +1,36 @@
 //define functions here
-// function getIt() {
-//   $('p').on('click', function() {
-//     alert("Hey!")
-//   })
-// }
-//
-// function frameIt() {
-//   $('img').on('load', function() {
-//     $(this).addClass('tasty')
-//   })
-// }
-//
-// function pressIt() {
-//   $('input').on('keydown', e => {
-//     const key = e.key
-//     if (key === "g"){
-//       alert('You pressed G')
-//     }
-//   })
-// }
-//
-// function submitIt() {
-//   $('form').on('submit', e => {
-//     alert("Your form is going to be submitted now.")
-//   })
-// }
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', e => {
+    const key = e.key
+    if (key === "g"){
+      alert('You pressed G')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', e => {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-  // getIt()
-  // frameIt()
+  getIt()
+  frameIt()
   // pressIt()
-  // submitIt()
+  submitIt()
 });
