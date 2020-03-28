187542dc39102e5dfb9fb9dde4feadc2e7bf0be7
Jackson Chen <jacksonchen171@gmail.com>
2020-03-28 21:40:56 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,30 +1,30 @@
 //define functions here
-function getIt() {
-  $('p').on('click', function() {
-    alert("Hey!")
-  })
-}
-
-function frameIt() {
-  $('img').on('load', function() {
-    $(this).addClass('tasty')
-  })
-}
-
-function pressIt() {
-  $('input').on('keydown', e => {
-    const key = e.key
-    if (key === "g"){
-      alert('You pressed G')
-    }
-  })
-}
-
-function submitIt() {
-  $('form').on('submit', e => {
-    alert("Your form is going to be submitted now.")
-  })
-}
+// function getIt() {
+//   $('p').on('click', function() {
+//     alert("Hey!")
+//   })
+// }
+//
+// function frameIt() {
+//   $('img').on('load', function() {
+//     $(this).addClass('tasty')
+//   })
+// }
+//
+// function pressIt() {
+//   $('input').on('keydown', e => {
+//     const key = e.key
+//     if (key === "g"){
+//       alert('You pressed G')
+//     }
+//   })
+// }
+//
+// function submitIt() {
+//   $('form').on('submit', e => {
+//     alert("Your form is going to be submitted now.")
+//   })
+// }
 
 $(document).ready(function(){
 
