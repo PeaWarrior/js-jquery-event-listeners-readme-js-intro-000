62f624a5a89467ce28412ef436cf25f27c58e32a
Jackson Chen <jacksonchen171@gmail.com>
2020-03-28 21:46:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('.typing').on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if (key.which == 82){
       alert('g was pressed')
     }
